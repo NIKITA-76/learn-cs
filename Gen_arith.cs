94c258706a72cdd8a899c2ae451450a48a300d65
N76 <NIKITA-76>
2023-01-20 01:12:26 +0300

    Добавьте файлы проекта.

---
+++ Gen_arith.cs
@@ -0,0 +1,38 @@
+﻿
+
+namespace Lessons // Note: actual namespace depends on the project name.
+{
+    internal class Gen_arith
+    {
+        static void Main()
+        {
+            double result;
+            int numb;
+            int sum = 0;
+            int count = 0;
+            Console.WriteLine("Вводите числа для расчета. Как только закончите, нажмите Enter");
+            try
+            {
+                while (true)
+                {
+                    numb = Convert.ToInt32(Console.ReadLine());
+                    sum += numb;
+                    count++;
+                }
+            }
+            catch (Exception ex) 
+            {
+                result = (double) sum / count;
+                Console.WriteLine(result);
+            
+            }
+
+
+
+            
+
+        }
+
+
+    }
+}
