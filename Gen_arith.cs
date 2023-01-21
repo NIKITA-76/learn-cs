9b142a02d7a2396c7286ad5f2923b11f499f367f
NIKITA-76 <89522179992.ru@gmail.com>
2023-01-21 16:42:25 +0300

    new fun - sum_and_times

--- Gen_arith.cs
+++ Gen_arith.cs
@@ -6,6 +6,68 @@ namespace Lessons // Note: actual namespace depends on the project name.
     {
         static void Main()
         {
+
+            string choose;
+            Console.WriteLine("Выберите операцию");
+            Console.WriteLine("1 - gen_arith");
+            Console.WriteLine("2 - sum_and_times");
+            choose = Console.ReadLine();
+            if (choose == "1")
+            {
+                gen_arith();
+            }
+            else if(choose == "2")
+            {
+                sum_and_times();
+            }
+            else
+            {
+                Main();
+            }
+
+            
+
+        }
+
+        static void sum_and_times()
+        {
+                var list_of_num = new List<int>();
+                int accum = 0;
+                int sum = 0;
+                int sum_t = 1;
+                Console.WriteLine("Введите числа для обработки через Enter. После завершения ввода чисел, нажмите Enter");
+
+                try
+                {
+                    while (true)
+                    {
+                        accum++;
+                        list_of_num.Add(Convert.ToInt32(Console.ReadLine()));
+
+
+                        sum++;
+                    }
+                }
+                catch
+                {
+                    foreach (int i in list_of_num)
+                    {
+
+                        sum += i;
+                        sum_t *= i;
+
+                    }
+                    Console.WriteLine("Сумма всех чисел --- > " + sum);
+                    Console.WriteLine("Произведение всех чисел --- > " + sum_t);
+
+                }
+
+        }
+
+
+
+        static void gen_arith()
+        {
             double result;
             int numb;
             int sum = 0;
@@ -20,28 +82,26 @@ namespace Lessons // Note: actual namespace depends on the project name.
                     count++;
                 }
             }
-            catch (Exception ex) 
+            catch (Exception ex)
             {
-                if (count != 0) 
+                if (count != 0)
                 {
-                    result = (double) sum / count;
+                    result = (double)sum / count;
                     Console.WriteLine(result);
                 }
-                else 
+                else
                 {
                     Console.WriteLine("Вы не ввели некорректные данные !!");
                     Main();
-                
+
                 }
 
-            
+
             }
 
 
-
-            
-
         }
+
 
 
     }
